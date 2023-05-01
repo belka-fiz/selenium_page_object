44c98c12d432a98825a09a312e84e202bdd83ebe
alekseylantsov <lalex239@gmail.com>
2023-05-01 23:50:16 +0400

    docstrings

--- main_page.py
+++ main_page.py
@@ -4,6 +4,9 @@ from .base_page import BasePage
 
 
 class MainPage(BasePage):
+    """Class for Main page of the store"""
+
     def go_to_login_page(self):
+        """Click login link"""
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
--- test_main_page.py
+++ test_main_page.py
@@ -2,6 +2,7 @@ from .main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser, browser_name):
+    """Ensure a user can go from Main page to login page"""
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
