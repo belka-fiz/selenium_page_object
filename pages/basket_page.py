e36d68e876711001d61eb37928d7490b5ff6411b
alekseylantsov <lalex239@gmail.com>
2023-05-04 17:45:57 +0400

    linter fixes: docstrings

--- pages/basket_page.py
+++ pages/basket_page.py
@@ -6,8 +6,10 @@ class BasketPage(BasePage):
     """Page Object class for Basket page"""
 
     def should_be_empty(self):
+        """Verify that the basket is empty. Check the 'empty' label and that there are not items."""
         assert self.is_element_present(*BasketPageLocators.EMPTY_BASKET_LABEL), "No label that basket is empty"
         assert self.is_not_element_present(*BasketPageLocators.BASKET_ITEMS), "There are items in the basket"
 
     def should_be_basket_page(self):
+        """Verify that the page opened is the basket page"""
         assert self.url_contains("basket"), f"It is not basket page. Actual URL: {self.browser.current_url}"
